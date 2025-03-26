a1763728c9d25fd7b63c0302c105a6e0638b3294
alexeyvlasov2021 <78164716+alexeyvlasov2021@users.noreply.github.com>
2025-03-26 19:06:14 +0100

    removed animation for mobile devices
    found poor compatibility with Android devices

--- index.js
+++ index.js
@@ -3,115 +3,118 @@ document.addEventListener("DOMContentLoaded", (event) => {
     // gsap code here!
     gsap.registerPlugin(ScrollTrigger)
 
-    const animate_book = () => {
-        gsap.to('.book embed', {
-            x: 150,
-            rotate: '360deg',
-            duration: 0.4,
-            ease: "none",
-        })
+    let mm = gsap.matchMedia();
+    mm.add("(min-width: 1250px)", () => {
 
-        gsap.to('.book p', {
-            x: -80,
-            duration: 0.4,
-            ease: "none",
-        })
-    }
+        const animate_book = () => {
+            gsap.to('.book embed', {
+                x: 150,
+                rotate: '360deg',
+                duration: 0.4,
+                ease: "none",
+            })
 
-    const reverse_animate_book = () => {
-        gsap.to('.book embed', {
-            x: -20,
-            rotate: '-360deg',
-            duration: 0.4,
-            ease: "none",
-        })
-
-        gsap.to('.book p', {
-            x: 20,
-            duration: 0.4,
-            ease: "none",
-        })
-    }
-
-
-    const book_btn = document.querySelector('div.book')
-    book_btn.addEventListener('mousemove', animate_book)
-    book_btn.addEventListener('mouseleave', reverse_animate_book)
-
-
-    const tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.block',
-            start: 'top 50%',
-            end: 'top 30%',
-            // markers: true,
-            // scrub: 3,
-            duration: 3,
+            gsap.to('.book p', {
+                x: -80,
+                duration: 0.4,
+                ease: "none",
+            })
         }
-    })
 
-    tl.fromTo('.inner', { y: 200, opacity: 0 }, { y: 0, opacity: 1 })
+        const reverse_animate_book = () => {
+            gsap.to('.book embed', {
+                x: -20,
+                rotate: '-360deg',
+                duration: 0.4,
+                ease: "none",
+            })
 
-    gsap.to('.menu-container img', {
-        rotate: '360deg',
-        duration: 10,
-        repeat: -1,
-        yoyo: true,
-        ease: 'none',
-    })
+            gsap.to('.book p', {
+                x: 20,
+                duration: 0.4,
+                ease: "none",
+            })
+        }
 
-    const images = [
-        './images/reviews1.png',
-        './images/reviews2.png',
-        './images/reviews3.png',
-    ]
 
-    let index = 1
+        const book_btn = document.querySelector('div.book')
+        book_btn.addEventListener('mousemove', animate_book)
+        book_btn.addEventListener('mouseleave', reverse_animate_book)
 
-    const img_element = document.querySelector('.img-container img')
 
-    const changeImage = () => {
-        gsap.to(img_element, {
-            opacity: 0,
-            duration: 0.5,
-            onComplete: () => {
-                gsap.to(img_element, {
-                    opacity: 1,
-                    duration: 0.5,
-                    onStart: () => {
-                        img_element.src = images[index]
-
-                        if (index < images.length - 1) {
-                            index++
-                        }
-                        else {
-                            index = 0
-                        }
-                    }
-                })
-            },
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.block',
+                start: 'top 50%',
+                end: 'top 30%',
+                // markers: true,
+                // scrub: 3,
+                duration: 3,
+            }
         })
-    }
 
-    let interval;
+        tl.fromTo('.inner', { y: 200, opacity: 0 }, { y: 0, opacity: 1 })
 
-    // window.addEventListener("load", (event) => {
-    //     if(interval) clearInterval(interval)
-    //   });
+        gsap.to('.menu-container img', {
+            rotate: '360deg',
+            duration: 10,
+            repeat: -1,
+            yoyo: true,
+            ease: 'none',
+        })
 
-    interval = setInterval(changeImage, 5000)
+        const images = [
+            './images/reviews1.png',
+            './images/reviews2.png',
+            './images/reviews3.png',
+        ]
 
-    window.addEventListener("beforeunload", () => {
-        clearInterval(interval);
-    });
+        let index = 1
 
-    // ScrollTrigger.create({
-    //     // markers: true,
-    //     trigger: '.feedbacks',
-    //     onEnter: () => { interval = setInterval(changeImage, 3000) },
-    //     onEnterBack: () => { interval = setInterval(changeImage, 3000) },
-    //     onLeaveBack: () => clearInterval(interval),
-    //     onLeave: () => clearInterval(interval),
-    // });
+        const img_element = document.querySelector('.img-container img')
 
+        const changeImage = () => {
+            gsap.to(img_element, {
+                opacity: 0,
+                duration: 0.5,
+                onComplete: () => {
+                    gsap.to(img_element, {
+                        opacity: 1,
+                        duration: 0.5,
+                        onStart: () => {
+                            img_element.src = images[index]
+
+                            if (index < images.length - 1) {
+                                index++
+                            }
+                            else {
+                                index = 0
+                            }
+                        }
+                    })
+                },
+            })
+        }
+
+        let interval;
+
+        // window.addEventListener("load", (event) => {
+        //     if(interval) clearInterval(interval)
+        //   });
+
+        interval = setInterval(changeImage, 5000)
+
+        window.addEventListener("beforeunload", () => {
+            clearInterval(interval);
+        });
+
+        // ScrollTrigger.create({
+        //     // markers: true,
+        //     trigger: '.feedbacks',
+        //     onEnter: () => { interval = setInterval(changeImage, 3000) },
+        //     onEnterBack: () => { interval = setInterval(changeImage, 3000) },
+        //     onLeaveBack: () => clearInterval(interval),
+        //     onLeave: () => clearInterval(interval),
+        // });
+    })
 });
